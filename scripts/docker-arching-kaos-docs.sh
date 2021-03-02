42b9d897a711efc32a1d0abe99678dcf3bd3f666
kaotisk <kaotisk@arching-kaos.com>
2021-03-02 22:10:02 +0200

    Fixed to working startup and also do the variable thing

--- scripts/docker-arching-kaos-docs.sh
+++ scripts/docker-arching-kaos-docs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -e "port=3002:3000" --volume /arching-kaos-docs:/usr/local/docsify littlstar/docker-docsify --rm
+docker run -e "port=3002:3000" --volume {$ARCHING_KAOS_BASE_DIR}/docs:/usr/local/docsify littlstar/docker-docsify --rm
