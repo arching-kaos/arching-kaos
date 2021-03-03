ab79f141345607a7bdffa5a9555ac1e2f6d712d6
kaotisk <kaotisk@arching-kaos.com>
2021-03-04 01:28:01 +0200

    Getting docker starters ready

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+echo "Welcome to arching-kaos installation. This script is Fedora based."
+
+
+#TODO basic fedora install from the other repo
+#cjdroute-strip-comment.sh
+#data
+
+sh ./scripts/docker-arching-kaos-docs.sh
+sh ./scripts/docker-icecast.sh
+sh ./scripts/docker-ipfs.sh
+sh ./scripts/docker-opentracker.sh
+sh ./scripts/docker-ssb-create.sh
+#export
+
+#gen-playlist.sh
+#grep-env.sh
+#pwd.sh
+#run-dat-gateway.sh
+#run-dat.sh
+#sed-example.sh
