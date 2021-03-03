32a9d02c127729787624e026b8ed13020d728502
kaotisk <kaotisk@arching-kaos.com>
2021-03-03 15:51:49 +0200

    More on #2 for SSB, I think it's almost ready

--- scripts/docker-ssb-create.sh
+++ scripts/docker-ssb-create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker run -d --name sbot \
-   -v ~/ssb-pub-data/:/home/node/.ssb/ \
+   -v {$ARCHING_KAOS_BASE_DIR}/etc/ssb-pub-data/:/home/node/.ssb/ \
    -p 8008:8008 \
    -p 8989:8989 \
    --restart unless-stopped \
