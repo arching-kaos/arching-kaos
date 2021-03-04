11c48edd77a5cdca4bf9c0dbc2fcc672d5fa6504
kaotisk <kaotisk@arching-kaos.com>
2021-03-04 13:51:56 +0200

    Added healer for sbot

--- scripts/docker-ssb-create.sh
+++ scripts/docker-ssb-create.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Running sbot..."
 docker run -d --name sbot \
    -v {$ARCHING_KAOS_BASE_DIR}/etc/ssb-pub-data/:/home/node/.ssb/ \
    -p 8008:8008 \
@@ -6,3 +7,10 @@ docker run -d --name sbot \
    --restart unless-stopped \
    --memory 800m \
    ahdinosaur/ssb-pub
+echo "... done"
+echo "Running healer..."
+docker run -d --name healer \
+ -v /var/run/docker.sock:/tmp/docker.sock \
+ --restart unless-stopped \
+ ahdinosaur/healer
+echo "... done"
