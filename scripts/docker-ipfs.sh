706348a6b8bbb12799ca89d74401c2a88eb854e8
kaotisk <kaotisk@arching-kaos.com>
2021-03-04 08:39:33 +0200

    Changed path accordingly

--- scripts/docker-ipfs.sh
+++ scripts/docker-ipfs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -d --name ipfs -v {$ARCHING_KAOS_BASE_DIR}/ipfs/export:/export -v {$ARCHING_KAOS_BASE_DIR}/ipfs/data:/data/ipfs -p 4001:4001 -p 127.0.0.1:8080:8080 -p 127.0.0.1:5001:5001 ipfs/go-ipfs:latest
+docker run -d --name ipfs -v {$ARCHING_KAOS_BASE_DIR}/storage/ipfs/export:/export -v {$ARCHING_KAOS_BASE_DIR}/storage/ipfs/data:/data/ipfs -p 4001:4001 -p 127.0.0.1:8080:8080 -p 127.0.0.1:5001:5001 ipfs/go-ipfs:latest
