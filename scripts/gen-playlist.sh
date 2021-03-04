88974b48ca33220a984704867f51fba74c787642
kaotisk <kaotisk@arching-kaos.com>
2021-03-04 15:59:23 +0200

    Updated gen-playlist.sh script

--- scripts/gen-playlist.sh
+++ scripts/gen-playlist.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-head -1 /radio/current_playlist.m3u > genplay.m3u && find /dat | grep ogg >> genplay.m3u
-cp genplay.m3u /radio/current_playlist.m3u
+find $PWD/storage | grep ogg >> $PWD/storage/radio/current_playlist.m3u
