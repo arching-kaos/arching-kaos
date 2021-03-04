6b3528cb3aeacc1f8da712a5e15045272f642a00
kaotisk <kaotisk@arching-kaos.com>
2021-03-04 06:38:35 +0200

    More on configuration

--- configure.sh
+++ configure.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 echo "Getting the basics done..."
 
+sh ./scripts/configure-charybdis.sh
+sh ./scripts/configure-icecast.sh
+sh ./scripts/configure-liquidsoap.sh
+sh ./scripts/configure-nginx.sh
