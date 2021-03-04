2fbdc8d0af489d0ae63c906fe8032c1cfde63965
kaotisk <kaotisk@arching-kaos.com>
2021-03-04 07:21:47 +0200

    Fixed non-escaped slashes

--- scripts/configure-icecast.sh
+++ scripts/configure-icecast.sh
@@ -5,7 +5,7 @@ sed -i.bak -e 's/{$ICECAST_SOURCE_PASSWORD}/hackme/' etc/liquidsoap/radio.liq
 sed -i.bak -e 's/{$ICECAST_RELAY_PASSWORD}/hackme/' etc/icecast2/icecast.xml
 sed -i.bak -e 's/{$ICECAST_ADMIN_PASSWORD}/hackme/' etc/icecast2/icecast.xml
 sed -i.bak -e 's/{$ICECAST_HOSTNAME}/icecast.arching-kaos.com/' etc/icecast2/icecast.xml
-sed -i.bak -e 's/{$RADIO_WEBSITE_BASEURL}/http://radio.arching-kaos.com/' etc/icecast2/icecast.xml
+sed -i.bak -e 's/{$RADIO_WEBSITE_BASEURL}/http:\/\/radio.arching-kaos.com/' etc/icecast2/icecast.xml
 
 # Optional
 #sed -i.bak -e 's/{$ICECAST_PEER_IP}' etc/icecast2/icecast.xml
