b53d58fa74ae657b02010c39c66d6d26884a0c0b
kaotisk <kaotisk@arching-kaos.com>
2021-03-04 08:35:05 +0200

    Small rework to fit

--- scripts/docker-arching-kaos-docs.sh
+++ scripts/docker-arching-kaos-docs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -e "port=3002:3000" --volume {$ARCHING_KAOS_BASE_DIR}/docs:/usr/local/docsify littlstar/docker-docsify --rm
+docker run --name docs -d -e "port=3002:3000" --volume {$ARCHING_KAOS_BASE_DIR}/docs:/usr/local/docsify littlstar/docker-docsify --rm
