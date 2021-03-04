c95d536980e3e175a33d9a6ee03be60c0410f357
kaotisk <kaotisk@arching-kaos.com>
2021-03-04 08:43:01 +0200

    Changed path to storage

--- scripts/docker-liquidsoap.sh
+++ scripts/docker-liquidsoap.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -d --name liquidsoap --restart always -v {$ARCHING_KAOS_BASE_DIR}/etc/liquidsoap/radio.liq:/etc/liquidsoap/script.liq -v {$ARCHING_KAOS_BASE_DIR}/radio:/music pltnk/liquidsoap
+docker run -d --name liquidsoap --restart always -v {$ARCHING_KAOS_BASE_DIR}/etc/liquidsoap/radio.liq:/etc/liquidsoap/script.liq -v {$ARCHING_KAOS_BASE_DIR}/storage/radio:/radio pltnk/liquidsoap
