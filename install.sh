9c24cb77f9bea0592e6f1390973f134d56c4f0e3
kaotisk <kaotisk@arching-kaos.com>
2021-03-04 11:30:44 +0200

    Put some echoes to understand what's that

--- install.sh
+++ install.sh
@@ -5,11 +5,15 @@ echo "Welcome to arching-kaos installation."
 #TODO basic fedora install from the other repo
 #cjdroute-strip-comment.sh
 #data
-
+echo "Starting docs..."
 sh ./scripts/docker-arching-kaos-docs.sh
+echo "Starting icecast..."
 sh ./scripts/docker-icecast.sh
+echo "Starting ipfs..."
 sh ./scripts/docker-ipfs.sh
+echo "Starting opentracker..."
 sh ./scripts/docker-opentracker.sh
+echo "Starting ssb..."
 sh ./scripts/docker-ssb-create.sh
 #export
 
@@ -19,17 +23,20 @@ sh ./scripts/docker-ssb-create.sh
 #run-dat-gateway.sh
 #run-dat.sh
 #sed-example.sh
+echo "Starting liquidsoap..."
 sh ./scripts/docker-liquidsoap.sh
 
-
+echo "Starting API..."
 cd modules/arching-kaos-api
 ./install.sh
 cd ../..
 
+echo "Starting webpage..."
 cd modules/arching-kaos-radio
 ./start.sh
 cd ../..
 
+echo "Starting dat-store..."
 cd modules/docker-dat-store
 sh ./build.sh
 sh ./start.sh
