70ab9143626ba041cadc00c14ddfa937ce72c189
kaotisk <kaotisk@arching-kaos.com>
2021-03-04 08:21:05 +0200

    Added aka-module configuration

--- configure.sh
+++ configure.sh
@@ -12,8 +12,14 @@ git submodule update docker-dat-store
 cd ..
 
 echo "Configuring modules..."
+echo "\[docker-dat-store\]"
 sed -i.bak -e 's/{$PWD}/\home\/kaotisk\/projects\/arching-kaos/' modules/docker-dat-store/start.sh
 sed -i.bak -e 's/{$PWD}/\home\/kaotisk\/projects\/arching-kaos/' modules/docker-dat-store/start.sh
+echo "\[arching-kaos-api\]"
+sh ./scripts/configure-aka-module.sh
+
+
+
 
 echo "Configuring /etc ..."
 sh ./scripts/configure-charybdis.sh
---
+++ scripts/configure-aka-module.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sed -i.bak -e 's/{$HOME}/\/home\/kaotisk/' modules/arching-kaos-api/install.sh
+sed -i.bak -e 's/{$HOME}/\/home\/kaotisk/' modules/arching-kaos-api/install.sh
