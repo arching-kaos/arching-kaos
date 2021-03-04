d563b9d3832dfb437364d18639093f32acc68809
kaotisk <kaotisk@arching-kaos.com>
2021-03-04 07:25:08 +0200

    Duplicated line cause needed two time to configure correctly

--- scripts/fix-base-path.sh
+++ scripts/fix-base-path.sh
@@ -2,6 +2,7 @@
 sed -i.bak -e 's/{$ARCHING_KAOS_BASE_DIR}/\/home\/kaotisk\/projects\/arching-kaos/' scripts/docker-arching-kaos-docs.sh
 sed -i.bak -e 's/{$ARCHING_KAOS_BASE_DIR}/\/home\/kaotisk\/projects\/arching-kaos/' scripts/docker-icecast.sh
 sed -i.bak -e 's/{$ARCHING_KAOS_BASE_DIR}/\/home\/kaotisk\/projects\/arching-kaos/' scripts/docker-ipfs.sh
+sed -i.bak -e 's/{$ARCHING_KAOS_BASE_DIR}/\/home\/kaotisk\/projects\/arching-kaos/' scripts/docker-ipfs.sh
 sed -i.bak -e 's/{$ARCHING_KAOS_BASE_DIR}/\/home\/kaotisk\/projects\/arching-kaos/' scripts/docker-liquidsoap.sh
 sed -i.bak -e 's/{$ARCHING_KAOS_BASE_DIR}/\/home\/kaotisk\/projects\/arching-kaos/' scripts/docker-liquidsoap.sh
 sed -i.bak -e 's/{$ARCHING_KAOS_BASE_DIR}/\/home\/kaotisk\/projects\/arching-kaos/' scripts/docker-opentracker.sh
