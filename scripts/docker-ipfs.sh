581b7c1c570d2b06bbe384d34faa93657b866975
kaotisk <kaotisk@arching-kaos.com>
2021-08-19 00:59:54 +0300

    Creating a missing directory

--- scripts/docker-ipfs.sh
+++ scripts/docker-ipfs.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+mkdir -p $PWD/storage/ipfs/export
 podman run -d --restart always --name ipfs -v $PWD/storage/ipfs/export:/export -v $PWD/storage/ipfs/data:/data/ipfs -p 4001:4001 -p 127.0.0.1:8080:8080 -p 127.0.0.1:5001:5001 docker.io/ipfs/go-ipfs:latest
