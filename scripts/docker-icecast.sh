1eba28f01dd28a2e0636807d1880ae490655ce10
kaotisk <kaotisk@arching-kaos.com>
2021-08-24 19:47:14 +0300

    Changed icecast2 image since moul's is failing to run

--- scripts/docker-icecast.sh
+++ scripts/docker-icecast.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-podman run --pod arching-kaos --name icecast -d --restart always -v $PWD/etc/icecast2/icecast.xml:/etc/icecast2/icecast.xml docker.io/moul/icecast
+podman run --pod arching-kaos --name icecast -d --restart always -v $PWD/etc/icecast2/icecast.xml:/etc/icecast2/icecast.xml docker.io/infiniteproject/icecast
