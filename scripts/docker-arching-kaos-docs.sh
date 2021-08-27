6715d18884d866f67cbd66798f5fa99816eeea1f
kaotisk <kaotisk@arching-kaos.com>
2021-08-28 00:56:25 +0300

    Fixing issue with SELinux on docsify pod

--- scripts/docker-arching-kaos-docs.sh
+++ scripts/docker-arching-kaos-docs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-podman run --pod arching-kaos --name docs --restart always -d -v $PWD/docs:/usr/local/docsify docker.io/littlstar/docker-docsify
+podman run --pod arching-kaos --name docs --restart always -d -v $PWD/docs:/usr/local/docsify:Z docker.io/littlstar/docker-docsify
