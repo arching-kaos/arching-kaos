f0a005962f75f8d45bb238c81d006f39ba884303
kaotisk <kaotisk@arching-kaos.com>
2021-08-28 21:08:57 +0300

    [container] Fix SELinux problem with sharing local folder

--- scripts/docker-thelounge.sh
+++ scripts/docker-thelounge.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-podman run --pod arching-kaos -d --network=host \
+podman run --pod arching-kaos -d \
         --name thelounge \
-        --volume $PWD/etc/thelounge:/var/opt/thelounge \
+        --volume $PWD/etc/thelounge:/var/opt/thelounge:Z \
         --restart always \
 	docker.io/thelounge/thelounge:latest
