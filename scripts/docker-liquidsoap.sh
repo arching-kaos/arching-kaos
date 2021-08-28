e28eda5ea8a27677a263d73f287a55821fcfb113
kaotisk <kaotisk@arching-kaos.com>
2021-08-28 23:14:11 +0300

    [fix] SELinux hits the liquidsoap

--- scripts/docker-liquidsoap.sh
+++ scripts/docker-liquidsoap.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-podman run --pod arching-kaos -d --name liquidsoap --restart always -v $PWD/etc/liquidsoap/radio.liq:/etc/liquidsoap/script.liq -v $PWD/storage:/radio docker.io/pltnk/liquidsoap
+podman run --pod arching-kaos -d --name liquidsoap --restart always -v $PWD/etc/liquidsoap/radio.liq:/etc/liquidsoap/script.liq:Z -v $PWD/storage:/radio:Z docker.io/pltnk/liquidsoap
