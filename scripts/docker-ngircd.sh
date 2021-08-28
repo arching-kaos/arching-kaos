74d5b86e5164138b532e307e8eee79bac6c6c1d7
kaotisk <kaotisk@arching-kaos.com>
2021-08-28 20:57:21 +0300

    [irc] Packed the whole folder, instead of per file assign, resolved SELinux issue

--- scripts/docker-ngircd.sh
+++ scripts/docker-ngircd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-podman run -dt --name irc-server --pod arching-kaos -v $PWD/etc/ngircd/ngircd.conf:/etc/ngircd/ngircd.conf -v $PWD/etc/ngircd/ngircd.motd:/etc/ngircd/ngircd.motd docker.io/geekduck/ngircd
+podman run -dt --name irc-server --pod arching-kaos -v $PWD/etc/ngircd:/etc/ngircd:Z docker.io/geekduck/ngircd
