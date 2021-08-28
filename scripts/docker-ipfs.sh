b585b68d7022d54db9f82e92a75f679dbe238764
kaotisk <kaotisk@arching-kaos.com>
2021-08-28 21:32:28 +0300

    [container] Fixing IPFS SELinux runner

--- scripts/docker-ipfs.sh
+++ scripts/docker-ipfs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir -p $PWD/storage/ipfs/export
 mkdir -p $PWD/storage/ipfs/data
-podman run --pod arching-kaos -d --restart always --name ipfs -v $PWD/storage/ipfs/export:/export -v $PWD/storage/ipfs/data:/data/ipfs docker.io/ipfs/go-ipfs:latest
+podman run --pod arching-kaos -d --restart always --name ipfs -v $PWD/storage/ipfs/export:/export:Z -v $PWD/storage/ipfs/data:/data/ipfs:Z docker.io/ipfs/go-ipfs:latest
