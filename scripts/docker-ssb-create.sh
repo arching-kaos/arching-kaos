aff3c10584b98cd453dd8b58e838d36ad09cffc1
kaotisk <kaotisk@arching-kaos.com>
2021-08-31 01:26:38 +0300

    [ssb] SELinux (probably) fix [cleanup] Just moved multiline commands to one-liners

--- scripts/docker-ssb-create.sh
+++ scripts/docker-ssb-create.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 echo "Running sbot..."
-podman run --pod arching-kaos -d --name sbot \
-   -v $PWD/etc/ssb-pub-data/:/home/node/.ssb/ \
-   --memory 800m \
-   docker.io/ahdinosaur/ssb-pub
+podman run --pod arching-kaos -d --name sbot -v $PWD/etc/ssb-pub-data/:/home/node/.ssb/:Z --memory 800m docker.io/ahdinosaur/ssb-pub
 echo "... done"
 echo "Running healer..."
-podman run --pod arching-kaos -d --name healer \
- docker.io/ahdinosaur/healer
+podman run --pod arching-kaos -d --name healer docker.io/ahdinosaur/healer
 echo "... done"
