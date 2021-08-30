8f24499ad85ae4adbc588582f3d1a747b82ccbe1
kaotisk <kaotisk@arching-kaos.com>
2021-08-30 23:04:23 +0300

    [scripts] Modified gen-playlist.sh to not append but override the playlist file

--- scripts/gen-playlist.sh
+++ scripts/gen-playlist.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-find $PWD/storage | grep ogg >> $PWD/storage/current_playlist.m3u
+find $PWD/storage | grep ogg > $PWD/storage/current_playlist.m3u
+sed -i.bak -e 's/'$PWD'\/storage/\/radio/g' $PWD/storage/current_playlist.m3u
